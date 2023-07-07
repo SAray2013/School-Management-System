0fdc03dd4b9d21f2b9de7cf64491e340e42f4aa6
Ray <saraykim2013@gmail.com>
2023-07-07 18:18:15 +0700

    CRUD Course-Video

---
+++ src/main/java/com/tday/school_management_system/service/CourseVideoService.java
@@ -0,0 +1,18 @@
+package com.tday.school_management_system.service;
+
+import java.util.Map;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.tday.school_management_system.model.CourseVideo;
+import com.tday.school_management_system.dto.CourseVideoDisplayDTO;
+
+public interface CourseVideoService {
+	CourseVideo create(CourseVideo courseVideo);
+	CourseVideo getById(Long id);
+	Page<CourseVideo> getAll(Map<String, String> params);
+    List<CourseVideoDisplayDTO> toCourseVideoDisplayDTOs(List<CourseVideo> courseVideo);
+	CourseVideo update(Long id, CourseVideo courseVideo);
+	void delete(Long id);
+}
